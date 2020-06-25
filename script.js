0b9e94b5f051f4587a1544c917e73199c385498c
JackieInTheHouse <xzheng0203@hotmail.com>
2020-06-25 11:12:32 +0100

    Add draw game function and display wins or draw game

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 // bugs to be fixing:
-// 1 shouln't be able to click on opponent's field.
-// 2. write a draw game function.
+// 1 shouln't be able to click on opponent's field - done.
+// 2. write a draw game function- done.
 // 3. add a reset game button.
 // 4. add player to choose x or o maybe, but this will need to update some functions. e.g takeTure and aiTurn function.
 
@@ -20,21 +20,61 @@ const winningSequences = [
 
 // create a function for the array query of all cells. Array.from returns an array list
 const cells = () => Array.from(document.getElementsByClassName('cell'));
+
 // a helper function to convert string to int, and replace c with empty string.
 const cellNumId = (c) => Number.parseInt(c.id.replace('c', ''));
+
 //  a helper function to let AI know which cell is empty to select
 const cellEmpty = () => cells().filter(cell => cell.innerText === '');
+
 //  to check all 3 cells in an array is all have the same value to the first one as a winner, but not empty.
 const threeMatch = (array) => array.every(cell => cell.innerText === array[0].innerText && cell.innerText !== '');
-// put mark into the selected cell
-const takeTurn = (index, mark) => cells()[index].innerText = mark;
+
 // help function for AI to select empty cell. get random rumber times with empty cell length could be between 0-9, but Math floor can get it  down to 8, because cell started from 0,
 // then recursive check the empty cell with cellEmpty helper function, cellNumId convert string to int satisfy the takeTurn function's first parameter index.
 const aiChoice = ()=> cellNumId(cellEmpty()[Math.floor(Math.random()*cellEmpty().length)]);
+
+// check for draw game
+const drawGame = () => cells().every(cell => cell.innerText!== '');
+
+let turnSuccess = true;
+let myTurn = true;
+
+const takeTurn = (index, mark) => {
+  if(!cells()[index].innerText == ''){
+    turnSuccess = false;
+    return;
+  }
+  cells()[index].innerText = mark;
+
+  switch(mark){
+    case "O":
+      myTurn = true;
+      break;
+    case "X":
+      myTurn = false;
+      break;
+  }
+  turnSuccess = true;
+}
+
 // end game function
-const gameOver =(winningSequence) =>{
+const gameOver = (winningSequence) =>{
+  disableCellListeners();
+  let endGameMessage = document.getElementById("endGameMessage");
+  if (winningSequence === 'draw'){
+    endGameMessage.innerText = "It's a draw game!";
+    return;
+  }
+
   winningSequence.forEach(cell => cell.classList.add('winner'));
-  ;}
+  if(myTurn){
+    endGameMessage.innerText = "You win!";
+  }else if(!myTurn){
+    endGameMessage.innerText = "AI wins!";
+    }
+  }
+
 
 // check winner function, set winner into false as no winner, loop through each element 's' in winningSequences, set another const _cells equal to cells to be reused.
 //each winning sequence is in a sub array, take sub array's first index, second index and third index.
@@ -49,8 +89,16 @@ const gameWinner =() => {
       gameOver(sequence);
     }
   });
+
+  if(!winner){
+    if(drawGame()){
+      winner = true;
+      gameOver('draw');
+    }
+  }
   return winner;
 }
+
 // function for AI turn, set AI timeout to 1.5 second, aiChoice is for AI to select the empty cell
 const aiTurn = () => {
   disableCellListeners();
@@ -63,21 +111,9 @@ const aiTurn = () => {
 
 //function for player at game in progress. player is always O and AI is alwyas X, cellNumId convert string to int satisfy the takeTurn function's first parameter index.
 
-//should not able to click on the field has already been clicked. still fixing!!!
-// const cellfilled = () => cells().filter(cell => cell.innerText === 'x' || cell.innerText === 'o');
-// const eventClick =(event) => {
-//   const id = event.target.id;
-//   if (id.innerText === 'x' || id.innerText === 'o'){
-//     return;
-//   }
-//   takeTurn(cellNumId(event.target), 'o');
-//   if(!gameWinner())
-//   aiTurn();
-// };
-
-const eventClick =(event) => {
+const eventClick = (event) => {
  takeTurn(cellNumId(event.target), 'O');
-  if(!gameWinner())
+  if(!gameWinner() && turnSuccess)
   aiTurn();
 };
 
@@ -87,4 +123,3 @@ cellListeners();
 
 // disable the event listener
 const disableCellListeners = () => cells().forEach(ce => ce.removeEventListener('click', eventClick));
-// disableCellListeners();
